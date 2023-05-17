e74a40b0d91853e4a20bb65dfd8f9a1ea09af3c1
Andrey <planchet2013@gmail.com>
2023-05-17 22:07:06 +0300

    start create admin panel

---
+++ resources/views/admin/sponsor.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.main')
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Clubs count</th>
+                    <th>Photo</th>
+                </tr>
+                @foreach($sponsors as $sponsor)
+                    <tr>
+                        <td> {{$sponsor->name}}</td>
+                        <td> {{$sponsor->type}}</td>
+                        <td> {{$sponsor->clubs_count}}</td> {{--так как было подгружено только кол-во клубов, то используется так--}}
+                        <td><img  src="{{$sponsor->photo_url}}" alt="Photo" class="sponsor_img"></td>
+                    </tr>
+                @endforeach
+            </table>
+            <div class="href_pages">
+                <a href="{{route('clubs.index')}}">Клубы</a>
+            </div>
+        </div>
+    </div>
+
+@endsection
