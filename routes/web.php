fdbb9d4f22470c6febce03a9efd16da911a493ce
Andrey <planchet2013@gmail.com>
2023-06-01 22:56:40 +0300

    add logout logic

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('auth.login');
 });
 
 Route::resource('sponsors',SponsorController::class)->names('sponsors');
